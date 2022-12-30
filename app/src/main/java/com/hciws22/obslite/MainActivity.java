c5f5eeeeeefc7599158fe9cc794b599687db4afe
stmunguy <Minh.P.b.Nguyen@stud.h-da.de>
2022-12-30 22:02:22 +0100

    adding empty Settings + WEEK

--- app/src/main/java/com/hciws22/obslite/MainActivity.java
+++ app/src/main/java/com/hciws22/obslite/MainActivity.java
@@ -22,27 +22,31 @@ public class MainActivity extends AppCompatActivity {
         syncController.init(findViewById(R.id.send));
         moveToDo(findViewById(R.id.move));
         moveToday(findViewById(R.id.move_today));
+        moveSettings(findViewById(R.id.move_settings));
     }
 
     public void moveToDo(View moveBtn) {
-        moveBtn.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TodoActivity.class);
-                startActivity(intent);
-            }
+        moveBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TodoActivity.class);
+            startActivity(intent);
         });
     }
 
     public void moveToday(View moveBtn) {
-        moveBtn.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TodayActivity.class);
-                startActivity(intent);
-            }
+        moveBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TodayActivity.class);
+            startActivity(intent);
         });
     }
+
+    public void moveSettings(View moveBtn) {
+        moveBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
 
 }
 
---
+++ app/src/main/java/com/hciws22/obslite/WeekActivity.java
@@ -0,0 +1,14 @@
+package com.hciws22.obslite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.week);
+    }
+}
