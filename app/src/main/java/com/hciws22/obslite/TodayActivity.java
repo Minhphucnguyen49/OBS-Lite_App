ac52426edfb69ef8f7dd32843f20aba174c055ee
stmunguy <Minh.P.b.Nguyen@stud.h-da.de>
2022-12-30 15:14:53 +0100

    adding space between cards TODO + TODAY

--- app/src/main/java/com/hciws22/obslite/TodayActivity.java
+++ app/src/main/java/com/hciws22/obslite/TodayActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import com.hciws22.obslite.db.SqLiteHelper;
 import com.hciws22.obslite.today.LectureRecViewAdapter;
 import com.hciws22.obslite.today.TodayController;
+import com.hciws22.obslite.utils.SpacingItemDecorator;
 
 public class TodayActivity extends AppCompatActivity {
 
@@ -28,6 +29,11 @@ public class TodayActivity extends AppCompatActivity {
         modulesRecView.setLayoutManager(new LinearLayoutManager(this));
 
         adapter.setModules(todayController.getToDay());
+
+        //Add space between cards
+        SpacingItemDecorator itemDecorator = new SpacingItemDecorator(30);
+        modulesRecView.addItemDecoration(itemDecorator);
+
     }
 }
 
