e4aa2c196fc5a93b774beff5cd4e2b5cd3b1c5d5
Boujada, Jamil <jamil.boujada@mt-ag.com>
2022-12-31 11:04:31 +0100

    implementing autosync service, scheduling jobs for main, response and sync services

--- app/src/main/java/com/hciws22/obslite/MainActivity.java
+++ app/src/main/java/com/hciws22/obslite/MainActivity.java
@@ -7,19 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.hciws22.obslite.db.SqLiteHelper;
-import com.hciws22.obslite.sync.SyncController;
 
 public class MainActivity extends AppCompatActivity {
-
-    SyncController syncController = new SyncController(new SqLiteHelper(MainActivity.this));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        syncController.init(findViewById(R.id.send));
         moveToDo(findViewById(R.id.move));
         moveToday(findViewById(R.id.move_today));
         moveSettings(findViewById(R.id.move_settings));
--- app/src/main/java/com/hciws22/obslite/jobs/ResponseService.java
+++ app/src/main/java/com/hciws22/obslite/jobs/ResponseService.java
@@ -1,7 +1,7 @@
-package com.hciws22.obslite.sync;
+package com.hciws22.obslite.jobs;
 
+import android.util.Log;
 import androidx.annotation.NonNull;
-
 import com.hciws22.obslite.enums.ContentTypeFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,30 +18,38 @@ public class ResponseService implements Callback {
 
     private final String SERVER_URI = "https://obs.fbi.h-da.de/obs/service.php?action=getPersPlanAbo&lfkey=a52b599e4590ef0dcebd9cb3cce4c069fb14723adb31161445ce8d31de3942cd998ef01c09f5ffac";
     private final OkHttpClient client = new OkHttpClient().newBuilder().build();
-    private final List<String> filteredList = new ArrayList <>();
+    private static final List<String> filteredList = new ArrayList<>();
+    private static boolean lock = true;
+
 
 
     public void getDataFromObs() throws IOException {
         Request request = requestSpecification();
         client.newCall(request).enqueue(this);
+
     }
 
     @Override
     public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
         e.printStackTrace();
     }
 
     @Override
     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+        lock = true;
         var bufferedReader = new BufferedReader(new InputStreamReader(response.body().source().inputStream()));
 
-        synchronized (this.filteredList) {
+        Log.d(Thread.currentThread().getName() + ": Response Service", "Response code " + response.code());
+        synchronized (filteredList) {
             filteredList.clear();
             bufferedReader
                     .lines()
                     .filter(ContentTypeFactory::isValid)
                     .forEach(filteredList::add);
+            lock = false;
         }
+        Log.d(Thread.currentThread().getName() + ": Received List", String.valueOf(filteredList.size()));
     }
 
 
@@ -54,11 +62,16 @@ public class ResponseService implements Callback {
     }
 
     public List<String> getFilteredList(){
+
+
+        Log.d("ResponseService: ", "wait");
+        while (lock){
+                System.out.println("I'm waiting...");
+        };
+
+        Log.d("ResponseService: ", "not wait anymore");
+        Log.d(Thread.currentThread().getName() + ": check for converting", String.valueOf(filteredList.size()));
         return filteredList;
     }
-
-
-
-
 
 }
