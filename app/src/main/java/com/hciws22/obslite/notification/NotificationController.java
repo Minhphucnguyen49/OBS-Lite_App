111e67012bd248f07c717b8484d0c967ef99c804
Boujada, Jamil <jamil.boujada@mt-ag.com>
2023-01-14 12:14:38 +0100

    add notifications sql statements

--- app/src/main/java/com/hciws22/obslite/notification/NotificationController.java
+++ app/src/main/java/com/hciws22/obslite/notification/NotificationController.java
@@ -7,14 +7,12 @@ import androidx.core.app.NotificationCompat;
 
 import com.hciws22.obslite.db.SqLiteHelper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class NotificationController {
 
     private final NotificationDbService notificationDbService;
     private final NotificationModel notificationModel;
-    private static final String channelId = "TestChannel";
     private final Context context;
 
     public NotificationController(SqLiteHelper sqLiteHelper, Context context) {
@@ -24,23 +22,16 @@ public class NotificationController {
         this.context = context;
     }
 
-
     public void createNotification(){
 
          List<Notification> notifications = notificationDbService.selectNotifications(true,false, false);
 
-         if(notifications.isEmpty()){
+         if(notifications.isEmpty() || Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
              return;
          }
 
          notificationModel.buildNotification(context);
-
-         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-             notificationModel.sendNotification(context);
-         }
-
-
+         notificationModel.sendNotification(context);
+         notificationDbService.removeNotifications(true,false,false);
     }
-
-
 }
--- app/src/main/java/com/hciws22/obslite/notification/NotificationDbService.java
+++ app/src/main/java/com/hciws22/obslite/notification/NotificationDbService.java
@@ -3,6 +3,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.hciws22.obslite.db.SqLiteHelper;
+import com.hciws22.obslite.todo.Todo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +24,18 @@ public class NotificationDbService {
         return "DELETE FROM " + TABLE_NOTIFICATION + ";";
     }
 
-    private String selectNotificationTableTemplate(int newAdded, int oldChanged,int oldDeleted ){
-        return "SELECT * FROM " + TABLE_NOTIFICATION + " n WHERE n." + COLUMNS_FOR_NOTIFICATION[2] + " = " + newAdded + " " +
+    private String whereCondition(int newAdded, int oldChanged,int oldDeleted){
+        return " n WHERE n." + COLUMNS_FOR_NOTIFICATION[2] + " = " + newAdded + " " +
                 "OR " + " n WHERE n." + COLUMNS_FOR_NOTIFICATION[3] + " = " + oldChanged + " " +
                 "OR " + " n WHERE n." + COLUMNS_FOR_NOTIFICATION[4] + " = " + oldDeleted + ";";
+    }
+
+    private String deleteFromNotificationTableTemplate(int newAdded, int oldChanged,int oldDeleted ){
+        return "DELETE FROM " + TABLE_NOTIFICATION + whereCondition(newAdded,oldChanged,oldDeleted);
+    }
+
+    private String selectNotificationTableTemplate(int newAdded, int oldChanged,int oldDeleted ) {
+        return "SELECT * FROM " + TABLE_NOTIFICATION + whereCondition(newAdded, oldChanged, oldDeleted);
     }
 
     private int convertBoolToInt(Boolean bool){
@@ -42,6 +51,33 @@ public class NotificationDbService {
         return name.substring(0,name.lastIndexOf(' '));
     }
 
+    public void truncateNotifications(){
+
+        SQLiteDatabase db = sqLiteHelper.getWritableDatabase();
+        try {
+            db.beginTransaction();
+            String sql = truncateNotificationTableTemplate();
+            db.execSQL(sql);
+            db.setTransactionSuccessful();
+
+        }finally {
+            db.endTransaction();
+            db.close();
+        }
+    }
+    public void removeNotifications(boolean newAdded, boolean oldChanged, boolean oldDeleted){
+        SQLiteDatabase db = sqLiteHelper.getWritableDatabase();
+        try {
+            db.beginTransaction();
+            String sql = deleteFromNotificationTableTemplate(convertBoolToInt(newAdded),convertBoolToInt(oldChanged),convertBoolToInt(oldDeleted));
+            db.execSQL(sql);
+            db.setTransactionSuccessful();
+
+        }finally {
+            db.endTransaction();
+            db.close();
+        }
+    }
     public List<Notification> selectNotifications(boolean newAdded, boolean oldChanged, boolean oldDeleted){
         String sql = selectNotificationTableTemplate(convertBoolToInt(newAdded), convertBoolToInt(oldChanged), convertBoolToInt(oldDeleted));
 
